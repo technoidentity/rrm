86a2a2e85db7bb946571e4f5f6387ecd1cb8e555
rajeshponnala <rajesh.ponnala@technoidentity.com>
2016-03-07 13:09:14 +0530

    manage client side routes on server side

--- src/rrm/core.clj
+++ src/rrm/core.clj
@@ -32,6 +32,37 @@
 (defroutes app-routes
   (GET "/" [] home)
   (GET "/citizens" [] normal-user)
+  (GET "/login" [] home)
+  (GET "/mutations" [] home)
+  (GET "/mutations/update/:id" [id] home)
+  (GET "/mutations/add" [] home)
+  (GET "/revenue" [] home)
+  (GET "/revenue/add" [] home)
+  (GET "/revenue/update/:id" [id] home)
+  (GET "/khasragirdwani" [] home)
+  (GET "/khasragirdwani/add" [] home)
+  (GET "/khasragirdwani/update/:id" [id] home)
+  (GET "/masavi" [] home)
+  (GET "/masavi/add" [] home)
+  (GET "/masavi/update/:id" [id] home)
+  (GET "/consolidation" [] home)
+  (GET "/consolidation/add" [] home)
+  (GET "/consolidation/update/:id" [id] home)
+  (GET "/fieldbook" [] home)
+  (GET "/fieldbook/add" [] home)
+  (GET "/fieldbook/update/:id" [id] home)
+  (GET "/misc" [] home)
+  (GET "/misc/add" [] home)
+  (GET "/misc/update/:id" [] home)
+  (GET "/o2register" [] home)
+  (GET "/o2register/add" [] home)
+  (GET "/o2register/update/:id" [id] home)
+  (GET "/o4register" [] home)
+  (GET "/o4register/add" [] home)
+  (GET "/o4register/update/:id" [id] home)
+  (GET "/o6register" [] home)
+  (GET "/o6register/add" [] home)
+  (GET "/o6register/update/:id" [id] home)
   (route/resources "/static")
   (route/not-found "<h1>Page not found</h1>"))
 
@@ -46,5 +77,5 @@
 (defn -main
   "Record Room Management System "
   [& args]
-  (jetty/run-jetty app {:port 8080
+  (jetty/run-jetty app {:port 8193
                         :join? false}))
